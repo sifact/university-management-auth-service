89e50d69ee9ed845af8f2dec9c9787e8d9add2b2
sifat <sifatami596@gmail.com>
2023-05-31 09:16:28 -0700

    interface and model added

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,29 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+type userModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const User = model<IUser, userModel>('User', userSchema)
