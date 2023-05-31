6bdb01e322232aa779fc7a911abd983cd928f4fa
sifat <sifatami596@gmail.com>
2023-05-31 05:17:52 -0700

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+export default app
