6488d78f9d039523e21763a7da13be1b02fcde2d
sifat <sifatami596@gmail.com>
2023-05-31 02:01:50 -0700

    [Done] cors, parser setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
